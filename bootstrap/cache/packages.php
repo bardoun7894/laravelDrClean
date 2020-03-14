c58db83efc375ae1094e8db16ff85c0287185511
drClean <bridal1wedding@gmail.com>
2020-03-14 15:56:26 +0100

    remove class adminlte

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -6,13 +6,6 @@
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
     ),
   ),
-  'jeroennoten/laravel-adminlte' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'JeroenNoten\\LaravelAdminLte\\AdminLteServiceProvider',
-    ),
-  ),
   'laravel/tinker' => 
   array (
     'providers' => 
